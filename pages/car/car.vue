f3b9d9540c8434960a6145db6e665cdb3c8fb22a
xiaoma_sky <1364735703@qq.com>
2021-12-06 14:40:30 +0800

    个人页面提交

---
+++ pages/car/car.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<text>{{text}}</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				text:'购物车'
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/person/person.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		<text>我是测试的个人提交页面</text>
+		<text>{{text}}</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				text:'个人'
+			}
+		},
+		methods: {
+				get() {
+					this.$nextTick(function(){
+						
+					})
+				}
+		}
+	}
+</script>
+
+<style scoped>
+
+
+</style>
