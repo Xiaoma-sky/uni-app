b052a559e4a2d9c0a56592e1758a7ff7ccc4d78a
xiaoma_sky <1364735703@qq.com>
2021-12-06 14:43:15 +0800

    商场页面增加提交

---
+++ pages/shop/shop.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<text>{{text}}</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				text:'购物'
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
